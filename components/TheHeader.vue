4b5df26da6c4f24bb85020b3b9af57c6d864965d
Jared Pike <jaredpike@users.noreply.github.com>
2018-07-01 16:33:21 -0400

    init

---
+++ components/TheHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <header class="page-header">
+    <div class="page-header__inner">
+      <nuxt-link to="/" class="page-header__logo">Jared Pike</nuxt-link>
+      <nav class="page-header__nav">
+        <ul class="page-header__list">
+          <nuxt-link to="/work" tag="li" class="page-header__item"><a class="page-header__link">Work</a></nuxt-link>
+          <nuxt-link to="/about" tag="li" class="page-header__item"><a class="page-header__link">About</a></nuxt-link>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.page-header {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 6rem 8rem;
+  pointer-events: none;
+
+  a {
+    pointer-events: initial;
+  }
+
+  &__inner {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__list {
+    display: flex;
+  }
+
+  &__item {
+    & + * {
+      margin-left: 4rem;
+    }
+
+    &.nuxt-link-active {
+      text-decoration: underline;
+    }
+  }
+
+  &__link {
+    font-size: 1.6rem;
+  }
+
+  &__logo {
+    font-size: 1.6rem;
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <TheHeader />
+    <main class="main">
+      <nuxt/>
+    </main>
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader";
+
+export default {
+  components: {
+    TheHeader
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/reset.scss";
+
+html {
+  font-size: 62.5%;
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+}
+
+body {
+  font-size: 1rem;
+  font-family: Helvetica;
+  overflow-y: scroll;
+}
+
+.main {
+  @extend %container;
+
+  margin-top: 10rem;
+}
+</style>
