58d73d565b5e7b8aeb43fd6b0b3d523b82793c8d
Jared Pike <jaredpike@users.noreply.github.com>
2018-07-04 13:21:56 -0400

    add autoprefixing

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,6 +65,7 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
+    postcss: [require("autoprefixer")()],
     extend(config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
