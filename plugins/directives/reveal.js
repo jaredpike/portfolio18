b92b5382d210ba77624e55feb2e7715092d06f76
Jared Pike <jaredpike@users.noreply.github.com>
2018-07-15 21:51:55 -0400

    set opacity to 0 before reveal

--- plugins/directives/reveal.js
+++ plugins/directives/reveal.js
@@ -5,13 +5,14 @@ import anime from "animejs";
 Vue.directive("reveal", {
   inserted: function(el) {
     const watcher = scrollMonitor.create(el);
+    el.style.opacity = 0;
     watcher.enterViewport(() => {
       watcher.destroy();
       anime({
         targets: el,
         opacity: [0, 1],
         duration: 800,
-        easing: "easeInOutQuart",
+        easing: "easeInOutCubic",
         translateY: [60, 0]
       });
     });
