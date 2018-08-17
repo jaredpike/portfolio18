f2654ab00cff175eceea35e3eb6832614a35eea3
Jared Pike <jaredpike@users.noreply.github.com>
2018-08-16 23:40:35 -0400

    update meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,11 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: pkg.description }
+      {
+        hid: "description",
+        name: "description",
+        content: "Jared Pike is a Brooklyn-based designer and developer."
+      }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
