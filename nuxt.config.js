a19a7f0343fb3e77a383866f2143ebce100a3d8f
Jared Pike <jaredpike@users.noreply.github.com>
2018-08-16 23:44:47 -0400

    disabling unused plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [{ src: "~plugins/three", ssr: false }, "plugins/directives/reveal"],
+  plugins: [],
 
   /*
   ** Nuxt.js modules
