abac6044845d7d796aa7b1722f305c17390f0b31
Jared Pike <jaredpike@users.noreply.github.com>
2020-01-09 14:38:19 -0500

    minimum loader visibility

--- components/TheLoader.vue
+++ components/TheLoader.vue
@@ -5,13 +5,16 @@
         <div class="loader__logo">
           <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
             <g class="stem">
-              <path d="M0 16V0H4V16H0Z" transform="translate(112) scale(16)"/>
+              <path d="M0 16V0H4V16H0Z" transform="translate(112) scale(16)"></path>
             </g>
             <g class="j">
-              <path d="M0 0C0 3.31329 2.68671 6 6 6V0H0Z" transform="translate(0 160) scale(16)"/>
+              <path d="M0 0C0 3.31329 2.68671 6 6 6V0H0Z" transform="translate(0 160) scale(16)"></path>
             </g>
             <g class="p">
-              <path d="M4 4.19048C4.0032 1.78971 2.2096 0 0 0V8C2.2096 8 4 6.21029 4 4.19048Z" transform="translate(192) scale(16)"/>
+              <path
+                d="M4 4.19048C4.0032 1.78971 2.2096 0 0 0V8C2.2096 8 4 6.21029 4 4.19048Z"
+                transform="translate(192) scale(16)"
+              ></path>
             </g>
           </svg>
         </div>
@@ -23,14 +26,26 @@
 <script>
 export default {
   data: () => ({
-    loading: false
+    loading: false,
+    seconds: 0
   }),
   methods: {
     start() {
       this.loading = true;
+      this.interval = setInterval(() => {
+        this.seconds++;
+      }, 1000);
     },
     finish() {
-      this.loading = false;
+      if (this.seconds >= 1) {
+        this.seconds = 0;
+        this.loading = false;
+      } else {
+        setTimeout(() => {
+          this.loading = false;
+        }, 1000);
+      }
+      clearInterval(this.interval);
     }
   }
 };
